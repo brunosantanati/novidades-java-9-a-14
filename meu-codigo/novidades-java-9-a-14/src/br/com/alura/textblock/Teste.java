b7c644c6ef7ad6e21ac94a95e9ef7620aeed8a91
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-04-12 05:28:14 -0300

    Exemplo usando Bloco de Texto (Text Block)

--- meu-codigo/novidades-java-9-a-14/src/br/com/alura/textblock/Teste.java
+++ meu-codigo/novidades-java-9-a-14/src/br/com/alura/textblock/Teste.java
@@ -2,6 +2,7 @@ package br.com.alura.textblock;
 
 public class Teste {
 
+	//Novidade do Java 13 - Text Blocks.
 	public static void main(String[] args) {
 		String texto = "\"Lorem ipsum dolor sit amet, \n" + "consectetur adipiscing elit, sed do eiusmod tempor \n"
 				+ "incididunt ut labore et dolore magna aliqua. \n"
@@ -12,5 +13,21 @@ public class Teste {
 				+ "nulla pariatur. Excepteur sint occaecat cupidatat \n"
 				+ "non proident, sunt in culpa qui officia deserunt \n" + "mollit anim id est laborum.\"";
 		System.out.println(texto);
+		
+		System.out.println();
+		
+		String textoFormatado = """
+				"Lorem ipsum dolor sit amet, 
+				consectetur adipiscing elit, sed do eiusmod tempor 
+				incididunt ut labore et dolore magna aliqua. 
+				Ut enim ad minim veniam, quis nostrud exercitation 
+				ullamco laboris nisi ut aliquip ex ea commodo 
+				consequat. Duis aute irure dolor in reprehenderit 
+				in voluptate velit esse cillum dolore eu fugiat 
+				nulla pariatur. Excepteur sint occaecat cupidatat 
+				non proident, sunt in culpa qui officia deserunt 
+				mollit anim id est laborum."
+				""";
+		System.out.println(textoFormatado);
 	}
 }
