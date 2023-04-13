9ec42acc482a8fd830e04e20ea206d3a6656cc3f
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-04-12 23:12:01 -0300

    Executar requisicao assincrona com API HTTP/2 Client

--- meu-codigo/capitulo-3/req-assincrona-http2/src/br/com/alura/dao/PlanetaDAO.java
+++ meu-codigo/capitulo-3/req-assincrona-http2/src/br/com/alura/dao/PlanetaDAO.java
@@ -5,18 +5,32 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
 public class PlanetaDAO {
 
 	public void listar() throws URISyntaxException, IOException, InterruptedException {
+		/*
+		 * HttpClient httpClient = HttpClient.newBuilder().build();
+		 * 
+		 * HttpResponse<String> send = httpClient.send( HttpRequest.newBuilder().uri(new
+		 * URI("http://localhost:8080/planetas")).GET().build(),
+		 * BodyHandlers.ofString());
+		 * 
+		 * System.out.println(send.body());
+		 */
+
+		// Fazendo requisição assíncrona
 		HttpClient httpClient = HttpClient.newBuilder().build();
 
-		HttpResponse<String> send = httpClient.send(
-				HttpRequest.newBuilder().uri(new URI("http://localhost:8080/planetas")).GET().build(),
-				BodyHandlers.ofString());
-
-		System.out.println(send.body());
+		httpClient.sendAsync(HttpRequest.newBuilder().uri(new URI("http://localhost:8080/planetas")).GET().build(),
+				BodyHandlers.ofString()).whenComplete((s, t) -> System.out.println(s.body())); // poderia guardar o
+																								// retorno de sendAsync
+																								// em uma variável do
+																								// tipo
+																								// CompletableFuture<HttpResponse<String>>,
+																								// mas como não vamos
+																								// precisar disso não
+																								// iremos fazer isso
 	}
 }
