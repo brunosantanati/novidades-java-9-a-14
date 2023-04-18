1423edb45bb3b9d1ad47c3daf7321794c175e0be
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-04-18 04:25:31 -0300

    Criar um pool de threads

--- meu-codigo/capitulo-4/novidades-java-reactive-streams/src/br/com/alura/TesteEnvioNaoBloqueante.java
+++ meu-codigo/capitulo-4/novidades-java-reactive-streams/src/br/com/alura/TesteEnvioNaoBloqueante.java
@@ -2,6 +2,8 @@ package br.com.alura;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 import br.com.alura.model.NotaFiscal;
@@ -11,12 +13,16 @@ public class TesteEnvioNaoBloqueante {
 
 	public static void main(String[] args) {
 		System.out.println("A thread do TesteEnvioNaoBloqueante é: " + Thread.currentThread().getName());
+		ExecutorService newFixedThreadPool = Executors.newFixedThreadPool(2);
 		NotaFiscal primeiraNotaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
-		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
+		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>(newFixedThreadPool, 1);
 		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
 		publisher.consume(nfwsc::enviar);
 		publisher.consume(data -> {
 			System.out.println("Outra thread: " + Thread.currentThread().getName());
+		});
+		publisher.consume(data -> {
+			System.out.println("Mais uma thread: " + Thread.currentThread().getName());
 		});
 		publisher.submit(primeiraNotaFiscal);
 		System.out.println("Você irá receber a nota fiscal no seu e-mail");
